7c7e20d760c6c413f329375573c1b3a40cd2d481
fouk <fuke@devsenses.com>
2015-02-11 16:36:49 +0700

    go

--- src/test/HelloCI.Tests/Test.cs
+++ src/test/HelloCI.Tests/Test.cs
@@ -6,7 +6,7 @@ namespace HelloCI.Tests
     public class Test : AssertionHelper
     {
         [Test]
-        public void Test1()
+        public void Test1()   
         { 
             Expect(true); 
         }   
